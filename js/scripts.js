5244783da502c6337ba844d24c9acf4770359f41
herr-code <herrwebj@gmail.com>
2022-03-24 18:27:21 -0600

    Tabulaciones

--- js/scripts.js
+++ js/scripts.js
@@ -5,51 +5,51 @@ const pokeImg = document.getElementById('pokeImg');
 
 // Apis y programacion asincrona
 const fetchPokemon = () => {
-	const pokeName = document.getElementById('pokeName');
-	let pokeInput = pokeName.value.toLowerCase();
-	const url = `https://pokeapi.co/api/v2/pokemon/${pokeInput}`;
-	
-	fetch(url).then(res => {
-		//console.log(res)=>Promesa
-		if (res.status != 200) {
-			console.log(res);
-			pokeImage("./assets/img/ball96.png");
-			divElement.innerHTML = "No encontrado";
-		}else {
-			return res.json();
-		}
-	})
-	.then((data) => {
-		console.log(data);
-		// let pokeImg = data.sprites;
-		let pokeImg = data.sprites.front_default;
-		console.log(pokeImg);
-		pokeImage(pokeImg);
+  const pokeName = document.getElementById('pokeName');
+  let pokeInput = pokeName.value.toLowerCase();
+  const url = `https://pokeapi.co/api/v2/pokemon/${pokeInput}`;
 
-		let name = data.name;
-		let types = data.types;
-		let moves =  data.moves;
-		console.log(name, types, moves);
-		console.log(types[0].type.name);
-		divInfo(name, types, moves)
-	});
+  fetch(url).then(res => {
+    //console.log(res)=>Promesa
+    if (res.status != 200) {
+      console.log(res);
+        pokeImage("./assets/img/ball96.png");
+	divElement.innerHTML = "No encontrado";
+	}else {
+	  return res.json();
+	}
+  })
+  .then((data) => {
+    console.log(data);
+    // let pokeImg = data.sprites;
+    let pokeImg = data.sprites.front_default;
+    console.log(pokeImg);
+    pokeImage(pokeImg);
+
+    let name = data.name;
+    let types = data.types;
+    let moves =  data.moves;
+    console.log(name, types, moves);
+    console.log(types[0].type.name);
+    divInfo(name, types, moves)
+  });
 }
 
 const pokeImage = (url) => {
-	pokeImg.src = url;	
+  pokeImg.src = url;	
 } 
 
 const divInfo = (name, types, moves) => {
-	divElement.innerHTML = `<p>Nombre: ${name}</p>`;
-	types.forEach((element) => {
-		divElement.innerHTML += `<p>Tipo: ${element.type.name}</p>` });
-	moves.forEach((element) => {
-		divElement.innerHTML += `<p>${element.move.name}</p>`; });
-
-	console.log("NAme")
+  divElement.innerHTML = `<p>Nombre: ${name}</p>`;
+  
+  types.forEach((element) => {
+    divElement.innerHTML += `<p>Tipo: ${element.type.name}</p>` });
+    moves.forEach((element) => {
+    divElement.innerHTML += `<p>${element.move.name}</p>`; 
+  });
 }
 
 // Accion
 button.addEventListener('click', () => {
-	fetchPokemon();
+  fetchPokemon();
 });
